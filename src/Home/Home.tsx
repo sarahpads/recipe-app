6a99d6c6c95c155d81187a15ef6e21760c71e3a9
Sarah Padovani <sarahpads@gmail.com>
2021-03-02 08:27:51 -0400

    grid layout

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { gql } from "graphql-tag"
 import { useQuery } from "@apollo/client"
-import RecipeCard from "../Recipe/RecipeCard/RecipeCard"
+import RecipeCard from "./RecipeCard/RecipeCard"
+import { Grid } from "@material-ui/core"
 
 const query = gql`
   query {
@@ -22,16 +23,22 @@ const Home: React.FC = () => {
     return <div>Error</div>
   }
 
-  return data.recipes.map((recipe: any) => (
-    <RecipeCard
-      id={recipe.id}
-      title={recipe.title}
-      image={recipe.image}
-      duration={recipe.duration}
-      description={recipe.description}
-      isFavourite={recipe.isFavourite}
-    />
-  ))
+  return (
+    <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={2}>
+      {data.recipes.map((recipe: any) => (
+        <Grid item xs={4} key={recipe.id}>
+          <RecipeCard
+            id={recipe.id}
+            title={recipe.title}
+            image={recipe.image}
+            duration={recipe.duration}
+            description={recipe.description}
+            isFavourite={recipe.isFavourite}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  )
 }
 
 export default Home
